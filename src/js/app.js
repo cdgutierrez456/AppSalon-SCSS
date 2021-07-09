1b660991304da0658e692c3cef8939231969d679
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-07-09 10:35:59 -0500

    Creando html de servicios. Estilizando servicios

--- src/js/app.js
+++ src/js/app.js
@@ -17,6 +17,29 @@ async function mostrarServicios() {
         // Generando html
         servicios.forEach(servicio => {
             const { id, nombre, precio } = servicio;
+
+            // DOM Scripting
+
+            // Generando nombre servicio
+            const nombreServicio = document.createElement('P');
+            nombreServicio.textContent = nombre;
+            nombreServicio.classList.add('nombre-servicio');
+
+            // Generando precio servicio
+            const precioServicio = document.createElement('P');
+            precioServicio.textContent = `$ ${precio}`;
+            precioServicio.classList.add('precio-servicio');
+
+            // Generar div contenedor de servicio
+            const servicioDiv = document.createElement('DIV');
+            servicioDiv.classList.add('servicio');
+
+            // Inyectar nombre y precio al div del servicio
+            servicioDiv.appendChild(nombreServicio);
+            servicioDiv.appendChild(precioServicio);
+
+            // Inyectarlo en el html
+            document.querySelector('#servicios').appendChild(servicioDiv);
         });
 
     } catch (error) {
